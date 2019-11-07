5afcab33dafc34202439256fd0323227725666d2
Dominic Tremblay <dominictremblay24@gmail.com>
2019-11-07 16:02:00 -0500

    added herko config for axios

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import axios from 'axios';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
